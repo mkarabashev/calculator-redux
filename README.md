00c2e993a9e0db704048d4a28ce4de55bce12762
mkarabashev <mkarabashev@gmail.com>
2017-06-08 14:40:28 +0300

    gh-pages for the calculator

--- README.md
+++
@@ -1,2 +0,0 @@
-# A calculator made using react-redux
-It is small enough to fit on a mobile display and has the option to go back to previous equations.
---
+++ style.45ebc3ca97c097f8ef3c.js
@@ -0,0 +1,2 @@
+webpackJsonp([1,3],{0:function(n,o,c){c(53),n.exports=c(57)},53:function(n,o){}});
+//# sourceMappingURL=style.45ebc3ca97c097f8ef3c.js.map
